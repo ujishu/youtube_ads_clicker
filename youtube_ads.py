9df21b3f6c21ca237267145691c3817e51ebefa8
ujishu <ujishu2010@gmail.com>
2016-06-21 00:56:37 +0300

    remove phantomjs, added xvfb

--- youtube_ads.py
+++ youtube_ads.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
+from xvfbwrapper import Xvfb #import virtual display
+vdisplay = Xvfb()
+vdisplay.start()
 
 #Proxy search 
 
@@ -9,34 +13,52 @@ import time
 #profile.set_preference("network.proxy.http", '79.188.42.46')
 #profile.set_preference("network.proxy.http_port", '8080')
 #profile.update_preferences()
+#driver = webdriver.Firefox(firefox_profile=profile)
+
+############## PhantomJS ################################
+#PhantomJS webdriver conf.
+#dcap = dict(DesiredCapabilities.PHANTOMJS)
+#dcap["phantomjs.page.settings.userAgent"] = (
+#	"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/53 "
+#    "(KHTML, like Gecko) Chrome/15.0.87"
+#)
 
 #Set proxy for PhantomJS
-service_args = ['--proxy=176.37.38.90:80', '--proxy-type=http','--ignore-ssl-errors=true']
-driver = webdriver.PhantomJS(executable_path='/home/ujishu/phantomjs-2.1.1-linux-x86_64/bin/phantomjs' ,service_args=service_args)
-driver.get('http://httpbin.org/ip')
-driver.save_screenshot('/home/ujishu/httpbin.org.png')
-print(driver.page_source.encode('utf-8'))
+#service_args = ['--proxy=176.37.38.90:80', '--proxy-type=http','--ignore-ssl-errors=true', '--ssl-protocol=any']
+#service_args = []
+#driver = webdriver.PhantomJS(
+#	executable_path='/home/ujishu/phantomjs-2.1.1-linux-x86_64/bin/phantomjs' 
+#	,service_args=service_args
+#	,desired_capabilities=dcap
+#)
+#########################################################
 
-#driver = webdriver.Firefox(firefox_profile=profile)
-#driver.set_window_size(1120, 550)
-driver.get('https://www.youtube.com/watch?v=R6J3S18ewyE')
+####### IP checking ######
+#driver.get('http://httpbin.org/ip')
+#driver.save_screenshot('/home/ujishu/httpbin.org.png')
+#print(driver.page_source.encode('utf-8'))
+driver = webdriver.Firefox()
+#driver.set_window_size(1024, 768)
+driver.get('https://www.youtube.com/watch?v=_BI82YaqVU0')
+#driver.get('http://www.i.ua/')
 #driver.implicitly_wait(17)
-time.sleep(17)
-print('time.sleep complete')
-driver.save_screenshot('/home/ujishu/video.png')	
+time.sleep(20)
+#print('time.sleep complete')
+driver.save_screenshot('/home/ujishu/video.png')
+
 #click on Skip button (not work)
 #if driver.find_element_by_css_selector('.videoAdUiSkipContainer .html5-stop-propagation'):
 #	skip = driver.find_element_by_css_selector('.videoAdUiSkipContainer .html5-stop-propagation').click()
 
 # need more test. not all ads clickable 
-# Find and click ads on page and video
+# Find and click ads on page and video#############################################################
 try:
 #ad on video
 	imageContainer = driver.find_element_by_css_selector('.image-container').click()
 	print('Found ad on video')
 except:
 	print('ad on video not found, find_element_by_css_selector')
-time.sleep(3)
+time.sleep(5)
 
 try:
 #ad on video
@@ -44,7 +66,7 @@ try:
 	print('Found ad on video')
 except:
 	print('ad on video not found, find_element_by_class_name')
-time.sleep(3)
+time.sleep(5)
 
 try:
   #ad at right side, frame1
@@ -69,6 +91,7 @@ except:
 
 driver.quit()
 print('Driver.quit')
+vdisplay.stop()
 
 #################################################################
 
