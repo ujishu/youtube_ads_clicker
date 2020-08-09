c68eacfcf0a7dcc0257c3ea69b94ac56be7d5de7
ujishu <ujishu2010@gmail.com>
2020-08-09 11:15:18 +0300

    Added imports

--- ipCheck.py
+++ ipCheck.py
@@ -1,14 +1,14 @@
 from selenium import webdriver
 #from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import time
+import os
+import sys
 from xvfbwrapper import Xvfb
 
 vdisplay = Xvfb()
 vdisplay.start()
 
 driver = webdriver.Firefox()
-
-
 
 driver.quit()
 print('Driver.quit')
